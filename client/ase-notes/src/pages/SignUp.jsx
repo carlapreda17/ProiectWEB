1cbaced1042aa79a32b3012320b172cd9b3131c4
carla17preda <carla_preda@yahoo.com>
2023-12-16 21:50:40 +0200

    form validation + dropdown

--- client/ase-notes/src/pages/SignUp.jsx
+++ client/ase-notes/src/pages/SignUp.jsx
@@ -1,72 +1,138 @@
 import React from "react";
 import {useState} from "react";
 
-function SignUp(params){
-    const [valueNume, setValueNume] = useState('');
-    const [valuePrenume, setValuePrenume] = useState('');
-    const [valueMail, setValueMail] = useState('');
-    const [valueTelefon, setValueTelefon] = useState('');
-    const [valueFacultate, setValueFacultate] = useState('');
-    const [valueParola, setValueParola] = useState('');
 
-    const handleChangeNume = (event) => {
-        setValueNume(event.target.value);
+function SignUp(props){
+    const [fields, setFields] = useState({});
+    const [errors, setErrors] = useState({});
+    const [selectedFacultate, setSelectedFacultate] = useState("");
+
+    const handleValidation = () => {
+        const formFields = {...fields};
+        const formErrors = {};
+        let formIsValid = true;
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/;
+        const phoneRegex=/^07\d{8}$/;
+        const nameRegex=/^[a-zA-Z]+$/
+        const mailRegex=/^[^@\s]+@stud\.ase\.ro$/
+
+        //verificam daca fiecare field e completat
+        if(!formFields["nume"] && !formFields["prenume"] && !formFields["email"] && !formFields["parola"] && !formFields["telefon"] && !formFields["facultate"]){
+            formIsValid = false;
+            formErrors["nume"] = "Cannot be empty";
+            formErrors["prenume"] = "Cannot be empty"
+            formErrors["parola"] = "Cannot be empty"
+            formErrors["email"] = "Cannot be empty"
+            formErrors["telefon"] = "Cannot be empty"
+            formErrors["facultate"] = "Cannot be empty"
+        }
+
+        //fara numere in nume/prenume
+        if(typeof formFields["nume"] !== "undefined" && typeof formFields["prenume"] !== "undefined"){
+            if(!nameRegex.test(formFields["nume"] && !nameRegex.test(formFields["prenume"]))){
+                formIsValid = false;
+                formErrors["nume"] = "Only letters";
+                formErrors["prenume"] = "Only letters";
+            }
+        }
+
+        if(typeof formFields["email"] !=="undefined")
+        {
+            if(!mailRegex.test(formFields["email"]))
+            {
+                formIsValid = false;
+                formErrors["mail"] = "Invalid mail";
+            }
+        }
+
+        //parola
+        if(typeof formFields["parola"] !=="undefined"){
+            if(!passwordRegex.test(formFields["parola"]))
+            {
+                formIsValid=false;
+                formFields["parola"] ="Invalid password"
+            }
+        }
+
+        //nr telefon
+        if(typeof formFields["telefon"] !=="undefined"){
+            if(!phoneRegex.test(formFields["telefon"]))
+            {
+                formIsValid=false;
+                formFields["telefon"]="Incorrect number"
+            }
+        }
+
+        setErrors(formErrors)
+        return formIsValid;
+    }
+    const handleChange = (field, value) => {
+        setFields({
+            ...fields,
+            [field]: value
+        })
     }
 
-    const handleChangePrenume = (event) => {
-        setValuePrenume(event.target.value);
-    }
-    const handleChangeMail= (event) => {
-        setValueMail(event.target.value);
-    }
-
-    const handleChangeTelefon = (event) => {
-        setValueTelefon(event.target.value);
-    }
-    const handleChangeParola = (event) => {
-        setValueParola(event.target.value);
-    }
-    const handleChangeFacultate = (event) => {
-        setValueFacultate(event.target.value);
-    }
+    const handleSelectChange = (event) => {
+        setSelectedFacultate(event.target.value);
+    };
     const handleSubmit = async (event) => {
 
         const userData = {
-            nume: valueNume,
-            prenume: valuePrenume,
-            email: valueMail,
-            parola: valueParola,
-            telefon: valueTelefon,
-            facultate: valueFacultate
+            nume: fields["nume"],
+            prenume: fields["prenume"],
+            email: fields["email"],
+            parola: fields["parola"],
+            telefon: fields["telefon"],
+            facultate: selectedFacultate
         };
+
+        if(handleValidation())
+        {
+            alert("Form submitted")
+        }
+        else
+        {
+            alert("Errors")
+        }
         console.log(userData)
     }
     return(
+
         <form onSubmit={handleSubmit}>
             <label>
                 Name:
-                <input type="text" className={"border-2 border-main-pink"} value={valueNume} onChange={handleChangeNume} />
+                <input type="text"  className={"border-2 border-main-pink"} value={fields["nume"]} onChange={e=>handleChange("nume",e.target.value)}/>
+                <span>{errors["nume"]}</span>
             </label>
             <label>
                 Prenume:
-                <input type="text" className={"border-2 border-main-pink"} value={valuePrenume} onChange={handleChangePrenume} />
+                <input type="text" className={"border-2 border-main-pink"} value={fields["prenume"]} onChange={e=>handleChange("prenume",e.target.value)} />
+                <span>{errors["prenume"]}</span>
             </label>
             <label>
                 Mail:
-                <input type="email" className={"border-2 border-main-pink"} value={valueMail} onChange={handleChangeMail} />
+                <input type="email" className={"border-2 border-main-pink"} value={fields["email"]} onChange={e=>handleChange("email",e.target.value)} />
+                <span>{errors["email"]}</span>
             </label>
             <label>
                 Parola
-                <input type="password" className={"border-2 border-main-pink"} value={valueParola} onChange={handleChangeParola} />
+                <input type="password" className={"border-2 border-main-pink"} value={fields["parola"]} onChange={e=>handleChange("parola",e.target.value)} />
+                <span>{errors["parola"]}</span>
             </label>
             <label>
                 Telefon
-                <input type="number" className={"border-2 border-main-pink"} value={valueTelefon} onChange={handleChangeTelefon} />
+                <input className={"border-2 border-main-pink"} value={fields["telefon"]} onChange={e=>handleChange("telefon",e.target.value)} />
+                <span>{errors["telefon"]}</span>
             </label>
             <label>
                 Facultate:
-                <input type="text" className={"border-2 border-main-pink"} value={valueFacultate} onChange={handleChangeFacultate} />
             </label>
+            <select name="facultate" value={selectedFacultate} onChange={handleSelectChange}>
+                <option value="cibe">Cibernetica</option>
+                <option value="info">Informatica economica</option>
+                <option value="stat">Statistica</option>
+            </select>
             <input type="submit"  value="Submit" />
         </form>
     )
